389befdddbe5897167d606610e23af4f81d9accb
root <root@vagrant-centos65.vagrantup.com>
2014-08-11 18:17:56 +0900

    mod dir

--- chef-repo/solo.rb
+++ chef-repo/solo.rb
@@ -1,2 +1,2 @@
-#cookbook_path ["/var/chef/setup01/chef-repo/site-cookbooks"]
-cookbook_path ["/home/vagrant/work/chef-repo/site-cookbooks"]
+cookbook_path ["/var/chef/setup01/chef-repo/site-cookbooks"]
+#cookbook_path ["/home/vagrant/work/chef-repo/site-cookbooks"]
